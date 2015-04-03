1ecc4e4827137bdcd706557741b86bd1bcbeb3dd
diegopaez <diripar8@gmail.com>
2015-04-03 16:10:24 -0300

    rr analisis final

--- ADCanalisis/analisisEcg.py
+++ ADCanalisis/analisisEcg.py
@@ -8,5 +8,10 @@ Created on Thu Apr  2 15:10:08 2015
 import numpy as np
 import pyqtgraph as pg
 import matplotlib.pyplot as plt
-ecg = np.loadtxt('/home/ieb-ufsc/Escritorio/ADCi2c/ecg30seg.txt')
+
+ecg = np.loadtxt('/home/diegopaez/PycharmProjects/ADCanalisis/ecg30seg.txt')
 plt.plot(ecg)
+plt.show()
+
+#plt.fill(ecg, facecolor='blue', alpha=0.5)
+#fill_between
--- rranalisis/Final/finalAnalisis.py
+++ rranalisis/Final/finalAnalisis.py
@@ -10,8 +10,11 @@ from hrvclassA import hrvclass
 import numpy as np
 import pyqtgraph as pg
 
+import matplotlib.pyplot as plt
+
+
 #rr = np.loadtxt('/home/ieb-ufsc/PycharmProjects/rranalisis/Final/rrdiego14-May-14-23:48:12.txt')
-rr = np.loadtxt('/home/ieb-ufsc/PycharmProjects/rranalisis/Final/rrdiego214-May-14-23:54:04.txt')
+rr = np.loadtxt('/home/diegopaez/PycharmProjects/rranalisis/Final/rrdiego214-May-14-23:54:04.txt')
 
 hrvAnalisis = hrvclass(rr) # inicializa la clase
 rr_new = hrvAnalisis.filtrohrv(rr) # filter and resample the signal
@@ -25,7 +28,6 @@ print "NN50: ",  hrvAnalisis.NN50()
 print "pNN50: ", hrvAnalisis.pNN50() , "%"
 
 #analisis en frequencia
-
 Pxx,Fxx,aVLF,aLF,aHF,lfhf  = hrvAnalisis.freqDomainHRV(rr_new,fs = 4.0)
 
 print "VLF:", aVLF, "ms^2"
@@ -33,18 +35,28 @@ print "LF:", aLF, "ms^2"
 print "HF:", aHF, "ms^2"
 print "LF/HF:", "%.4f" % lfhf
 
-#np.savetxt('rrdiegoprocesado.txt', rr_new, fmt = '%i')
 
-#pg.plot(rr_new,pen=(200,200,200), symbolBrush=(255,0,0), symbolPen='w', symbolSize = 4)
-
-
-win = pg.GraphicsWindow(title="Diego Paez")
-win.resize(1000,600)
-win.setWindowTitle('HRV - PSD')
-
-p0 = win.addPlot(title = "PSD")
-p0.setXRange(0,0.6)
-p0.plot(Fxx,Pxx)
-p0.setLabel('left', "PSD", units='ms^2/Hz')
-p0.setLabel('bottom', "Frequency", units='Hz')
-
+#--------------------------------------------VFC
+#fig, ax = plt.subplots()
+fig, (ax,ax1) = plt.subplots(2,1)
+ax.plot(rr_new,color='black', linewidth = 2,marker='o',markersize=6,markerfacecolor=(1, 0, 0, 1))
+ax.set_xlabel("bpm")
+ax.set_ylabel("time [ms]")
+ax.grid(True)
+ax1.grid(True)
+#--------------------------------------------Freq
+#fig, ax1 = plt.subplots()
+ax1.plot(Fxx,Pxx,color='black',linewidth = "2")
+ax1.set_xlim(0,0.4)
+ax1.set_xlabel("Frequency (Hz)")
+ax1.set_ylabel("PSD (ms^2/Hz)")
+p3 = Rectangle((0, 0), 1, 1, fc=(0.49,1.0,1.0)) #fc = facecolor
+p2 = Rectangle((0, 0), 1, 1, fc=(0.69,0.49,1.0))
+p1 = Rectangle((0, 0), 1, 1, fc=(0.49,0.49,1.0))
+ax1.legend([p1, p2,p3], ["VLF","LF", "HF"])
+#
+ax1.fill_between(Fxx,Pxx,0,where=Fxx>=0.14, facecolor=(0.49,1.0,1.0), alpha=1)
+ax1.fill_between(Fxx,Pxx,0,where=Fxx<=0.15, facecolor=(0.69,0.49,1.0), alpha=1)
+ax1.fill_between(Fxx,Pxx,0,where=Fxx<=0.04, facecolor=(0.49,0.49,1.0), alpha=1)
+ax1.axvline(x=0.039,linewidth=4, color='r')
+ax1.axvline(x=0.1487,linewidth=4, color='r')
