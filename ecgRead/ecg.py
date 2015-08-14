834750316f7b9c1ec7af63553b5894c0464b5d78
diegopaez <diripar8@gmail.com>
2015-08-14 00:22:36 -0300

    test new signals

--- ecgRead/ecg.py
+++ ecgRead/ecg.py
@@ -4,13 +4,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy import signal
 
-ecg =np.loadtxt('/home/diegopaez/Escritorio/ecgADC/ecgRaw_1Khz.txt')
-Fs = 1000.0 # 250 Hz
+ecg =np.loadtxt('/home/diegopaez/PycharmProjects/ecgRead/ecgRaw_250Hz_4.txt')
+Fs = 250.0 # 250 Hz
 Vt = np.arange(0,(len(ecg))/Fs,1/Fs) # % 0 - 60 segundos, pasos de 1/250hz tasa de muestreo  
 
 
 Ord = 4.0           # Ordem do filtro
-Fc1 = 30.0         #F. corte analagica (FPB), Hz
+Fc1 = 40.0         #F. corte analagica (FPB), Hz
 W1c = Fc1/Fs
 #----------------------------------------------------
 # Coeficientes dos Filtros Butterworth
@@ -19,4 +19,7 @@ b, a = signal.butter(Ord, W1c*2, 'low') #FPB
 #----------------------------------------------------
 ecg_pb = signal.filtfilt(b, a, ecg) # aplicar filtro
 
+plt.figure(0)
+plt.plot(Vt,ecg)
+plt.figure(1)
 plt.plot(Vt,ecg_pb)
